31098f8a467854bc4fad08ad856ff7620d11b468
Cadufc91 <fernandes.cadu@gmail.com>
2022-08-19 14:41:25 -0300

    Funcionalidade adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
+import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import './styles.css';
+import api from './services/api';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState('');
+
+  async function handleSearch(){
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+    } catch{
+      alert('CEP inválido');
+      setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Busque o CEP</h1>
       <div className="container-input">
-        <input type="text" placeholder="Digite seu CEP..."/>
-        <button className='search-btn'><BsSearch size={25}/></button>
+        <input type="text" placeholder="Digite seu CEP..." value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button className='search-btn' onClick={handleSearch}><BsSearch size={25}/></button>
       </div>
-      <main className='main'>
-        <h2>CEP: </h2>
-        <span>Rua Teste</span>
-        <span>Complemento</span>
-        <span>Vila Rosa</span>
-        <span>Quissama - RJ</span>
-      </main>
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2 className='main-title'>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+      
     </div>
   );
 }
